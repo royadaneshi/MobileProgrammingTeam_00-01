e2344a099d0425b765ca03d8abd39cde5ca513c6
royadaneshi <royadaneshi2001@gmail.com>
2022-03-09 12:05:40 +0330

    first

--- app/src/main/java/com/edufire/rr/ProfessorFragment.java
+++ app/src/main/java/com/edufire/rr/ProfessorFragment.java
@@ -3,40 +3,58 @@ package com.edufire.rr;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfessorFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.edufire.rr.databinding.FragmentFirstBinding;
+
 public class ProfessorFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private Button registerProfessor;
+    private EditText username;
+    private EditText completeName;
+    private EditText universityName;
+    private EditText password;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_professor, container, false);
+
+        registerProfessor = view.findViewById(R.id.registerBtnProfesssor);
+        username = view.findViewById(R.id.usernameProfessor);
+        completeName = view.findViewById(R.id.completeNameProfessor);
+        universityName = view.findViewById(R.id.universityNameProfessor);
+        password = view.findViewById(R.id.passwordProfessor);
+
+        registerProfessor.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                new Professor(username.getText().toString(), password.getText().toString(), completeName.getText().toString(), universityName.getText().toString());
+                NavHostFragment.findNavController(ProfessorFragment.this)
+                        .navigate(R.id.action_mainActivity2_to_FirstFragment);
+            }
+        });
+        return view;
+
+    }
+
 
     public ProfessorFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SignUpFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static ProfessorFragment newInstance(String param1, String param2) {
         ProfessorFragment fragment = new ProfessorFragment();
         Bundle args = new Bundle();
@@ -55,10 +73,5 @@ public class ProfessorFragment extends Fragment {
         }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_professor, container, false);
-    }
+
 }
